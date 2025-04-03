25efcbdea30d0c65d83dcdc2663a37c7e1092677
Swann Julien - T026016 <swann.julien.contractor@bbva.com>
2025-04-03 13:29:56 +0200

    chore: initial commit

---
+++ src/main/java/net/booksnap/booksnap/BooksnapApplication.java
@@ -0,0 +1,13 @@
+package net.booksnap.booksnap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksnapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksnapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/booksnap/booksnap/BooksnapApplicationTests.java
@@ -0,0 +1,13 @@
+package net.booksnap.booksnap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksnapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
