f3311d1d84c99f81b7a6bf59bd48fb42949e15de
Swann Julien <swann.jn@gmail.com>
2025-08-25 23:15:36 +0200

    refactor: change call to repository in get all book's copies

--- src/main/java/net/booksnap/domain/copy/repository/CopyRepository.java
+++ src/main/java/net/booksnap/domain/copy/repository/CopyRepository.java
@@ -2,10 +2,18 @@ package net.booksnap.domain.copy.repository;
 
 import net.booksnap.domain.copy.Copy;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CopyRepository extends JpaRepository<Copy, Long> {
-     List<Copy> findAllByBookId(Long bookId);
+    
+    @Query("SELECT c FROM Copy c " +
+           "LEFT JOIN FETCH c.book " +
+           "LEFT JOIN FETCH c.library " +
+           "WHERE c.book.id = :bookId")
+    List<Copy> findAllByBookId(@Param("bookId") Long bookId);
+    
     // List<Copy> findByStatus(Status status);
 }
